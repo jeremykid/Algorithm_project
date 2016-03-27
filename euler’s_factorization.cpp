4d00b892b938d3d49bcfd6ad8df4d0dab851aeba
weijiesun <weijie2@ualberta.ca>
2016-03-27 16:19:10 -0600

    initial

---
+++ euler’s_factorization.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int number;
+	int testcases;
+	cin >> testcases;
+	while (testcases){
+		testcases--;
+		cin >> number;
+		
+		
+	}
+
+	return 0;
+}
---
+++ fermat’s_factorization.cpp
@@ -0,0 +1,21 @@
+//N = a**2 – b**2, then N = (a-b)(a+b)
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int testcases;
+	int number;
+	cin >> testcases;
+	while (testcases){
+		testcases--;
+		cin >> number;
+
+	}
+	
+	return 0;
+}
