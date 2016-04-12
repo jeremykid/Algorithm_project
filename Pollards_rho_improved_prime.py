957b964a5fd68394cc1278342ddae9be6a56c8a2
weijiesun <weijie2@ualberta.ca>
2016-04-12 02:01:59 -0600

    fix Pollard Miller

--- Pollards_rho_improved_prime.py
+++ Pollards_rho_improved_prime.py
@@ -4,6 +4,46 @@ import Miller_Rabin_primality_test
 import math
 import time
 
+def primeGenerate(number):
+    largest = number
+    # largest = int(math.sqrt(number)+1)
+    prime_list = largest*[1]
+    if (number<4):
+        return [2,3]
+    prime_list[1] = 0
+    for i in range(0,largest,2):
+        prime_list[i] = 0
+    
+    prime_list[2] = 1
+    for i in range(3,largest,2):
+        if (prime_list[i] == 1):
+            for j in range(2*i,largest,i):
+                prime_list[j] == 1
+            
+    result = []
+    for i in range(0,number):
+        if(prime_list[i] == 1):
+            result.append(i)
+
+    return result
+
+def trial_division(n):
+    """Return a list of the prime factors for a natural number."""
+    if n < 2:
+        return []
+    prime_factors_list = []
+    temp = primeGenerate(int(n**0.5) + 1)
+
+    for p in temp:
+        if p*p > n: 
+            break
+        while n % p == 0:
+            prime_factors_list.append(p)
+            n //= p
+    if n > 1:
+        prime_factors_list.append(n)
+    return prime_factors_list
+
 def gcd(a,b):
     while (b != 0):
         temp = a%b
@@ -27,12 +67,20 @@ def pollard(number):
             factor = gcd(x-x_fixed, number)
         cycle_size *= 2
         x_fixed = x
-    if (Miller_Rabin_primality_test.is_probable_prime(factor)):
+    if (factor == number):
+        prime_factors.extend(trial_division(factor))
+    elif (factor <= 3):
+        prime_factors.append(factor)
+    elif (Miller_Rabin_primality_test.is_probable_prime(factor)):
         prime_factors.append(factor)
     else:
         pollard(factor)
     another = number//factor
-    if (Miller_Rabin_primality_test.is_probable_prime(another)):
+    if (another == number):
+        prime_factors.extend(trial_division(another))
+    elif (another <= 3):
+        prime_factors.append(another)
+    elif (Miller_Rabin_primality_test.is_probable_prime(another)):
         prime_factors.append(another)
     else:
         pollard(another)
